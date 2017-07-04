bc3e4284c79198d8e158e04511ede66601096d2e
tuantvu <tuantvu@gmail.com>
2017-07-03 21:39:15 -0500

    initial android checkin

--- index.android.js
+++ index.android.js
@@ -3,51 +3,19 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {AppRegistry, ActivityIndicator} from 'react-native'
+import App from './src/app'
 
-export default class TheApp extends Component {
+//Surpress warnings by firebase's use of timers
+console.ignoredYellowBox = ['Setting a timer'];
+
+export default class TheApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <App />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TheApp', () => TheApp);
